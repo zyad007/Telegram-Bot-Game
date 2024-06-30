808eb3e001cbe206020379441187e21663ce91b6
Alaadel <alaadel@outlook.com>
2024-06-30 19:13:41 +0300

    comments

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require('express')
 
 const app = express()
 
+// used as a basic request that can be received to wake up the server
+// in this case on render using https://telegram-bot-game-2nl1.onrender.com/
 app.get('/', (req, res) => {
     res.send('Pong!')
 })
 
-// replace the value below with the Telegram token you receive from @BotFather
+// replace the values below with the Telegram data you receive from @BotFather
 const TOKEN = '7480365930:AAFEG4KbYFYgY2K7A834NSN71HYU5g4qkOo';
 const GAME_NAME = 'gameZyad';
 const GAME_URL = 'https://chipper-puffpuff-d03cbe.netlify.app/';
@@ -15,13 +17,12 @@ const GAME_URL = 'https://chipper-puffpuff-d03cbe.netlify.app/';
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(TOKEN, { polling: true });
 
+// define commands
 bot.setMyCommands([
     { command: '/start', description: 'Start the game' }
 ])
 
-
-
-// Matches "/echo [whatever]"
+// handle commands
 bot.onText(/\/start/, (msg) => {
     const chat_id = msg.chat.id;
     bot.sendGame(chat_id, GAME_NAME, {
@@ -32,19 +33,19 @@ bot.onText(/\/start/, (msg) => {
                         text: 'Play',
                         callback_game: GAME_URL
                     }
-                ],
-                [
-                    {
-                        text: "Yes",
-                        callback_data: "btn_yes",
-                        
-                    },
-                    {
-                        text: "No",
-                        callback_data: "btn_no"
-                    },
-
                 ]
+                // [UNCOMMENT TO ENABLE CUSTOM BUTTONS]
+                // ,[
+                //     {
+                //         text: "Yes",
+                //         callback_data: "btn_yes",
+                        
+                //     },
+                //     {
+                //         text: "No",
+                //         callback_data: "btn_no"
+                //     },
+                // ]
             ]
         }
     });
@@ -55,6 +56,7 @@ bot.on('callback_query', (c) => {
     console.log('Callback');
     console.log(c);
 
+    // get callback data
     const user = c.from;
     const user_id = user.id;
 
@@ -66,21 +68,24 @@ bot.on('callback_query', (c) => {
 
     console.log(`callback_query data: ${user.username}//${user_id}//${chat_instance}//${chat_id}//${game_name}`);
 
-    if(c.data === 'btn_yes') {
-        console.log('Yes');
-        return
-    }
+    // [UNCOMMENT TO ENABLE CUSTOM BUTTONS]
+    // handle buttons
+    // if(c.data === 'btn_yes') {
+    //     console.log('Yes');
+    //     return
+    // }
+    // if(c.data === 'btn_no') {
+    //     console.log('No');
+    //     return
+    // }
 
-    if(c.data === 'btn_no') {
-        console.log('No');
-        return
-    }
-
+    // reply with the game's url (so telegram can open it), along with data (so the game can use it to call the api later)
     // TODO: sanitize
     const url = `${GAME_URL}index.html?user_id=${user_id}&chat_id=${chat_id}&message_id=${message_id}`;
     return bot.answerCallbackQuery(c.id, { url: url })
 })
 
+// old sample code
 // // Listen for any kind of message. There are different kinds of
 // // messages.
 // bot.on('message', (msg) => {
@@ -89,7 +94,6 @@ bot.on('callback_query', (c) => {
 //     // send a message to the chat acknowledging receipt of their message
 //     bot.sendMessage(chatId, 'Received your message');
 // });
-
 
 app.listen(10000, () => {
     console.log(`Example app listening at http://localhost:${10000}`)
